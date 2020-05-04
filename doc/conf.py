aaa94242274919361a448a0b4668eddb30c7142a
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2020-05-04 15:20:16 +1000

    added pulling in docs from cogent3 repo into action

--- doc/conf.py
+++ doc/conf.py
@@ -1,156 +1,71 @@
-import os
-import shutil
-import sys
+import sphinx_bootstrap_theme
 
-from glob import glob
+project = "cogent3"
+copyright = "2020, cogent3 Team"
+author = "Gavin Huttley"
 
-from sphinx_gallery.sorting import ExplicitOrder, FileNameSortKey
+# The full version, including alpha/beta/rc tags
+release = ""
 
-# set the plotly renderer
-os.environ["PLOTLY_RENDERER"] = "sphinx_gallery"
 
-sys.path.append("../src")
+# -- General configuration ---------------------------------------------------
 
-# Allow autosummary to generate stub files
-autosummary_generate = True
-
-# Prevent numpydoc from requiring stub files for methods
-numpydoc_class_members_toctree = False
-
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    "numpydoc",
-    "sphinx.ext.todo",
-    "sphinx.ext.doctest",
-    "sphinx.ext.githubpages",
-    "nbsphinx",
     "sphinx.ext.mathjax",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
     "sphinxcontrib.bibtex",
-    "sphinx_gallery.gen_gallery",
+    "sphinx.ext.todo",
+    "sphinx.ext.githubpages",
 ]
 
-# todo_include_todos=True # to expose the TODOs, uncomment this line
-
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["templates"]
+templates_path = ["_templates"]
 
-# The suffix of source filenames.
-source_suffix = ".rst", ".ipynb"
-
-# ignore the cookbook/union_dict.rst file as it's specifically included
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
     "_build",
     "Thumbs.db",
     ".DS_Store",
     "**.ipynb_checkpoints",
-    "cookbook/union_dict.rst",
-    "draw_examples/README.rst",
-    "draw_examples/aln/README.rst",
-    "draw_examples/tree/README.rst",
 ]
 
-# The encoding of source files.
-# source_encoding = 'utf-8'
 
-# The master toctree document.
-master_doc = "index"
-
-# General information about the project.
-project = "cogent3"
-copyright = "2020, cogent3"
-
-release = "2020.2.7a"
-
-version = release
-
-# exclude_trees = ["_build"]
-
-show_authors = True
-
-pygments_style = "sphinx"
-
-# -- Options for HTML output ---------------------------------------------------
-
-html_theme = "alabaster"
-
-sidebar_collapse = True
-
-html_sidebars = {
-    "**": [
-        "about.html",
-        "navigation.html",
-        "relations.html",
-        "searchbox.html",
-        # 'donate.html',
-    ]
-}
+html_theme = "bootstrap"
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 
 html_theme_options = {
-    "fixed_sidebar": True,
+    "navbar_title": "cogent3",
+    "navbar_site_name": "Site",
+    "navbar_links": [
+        ("Install", "install"),
+        ("Docs", "https://cogent3.org/docs", True),
+        ("Code", "https://github.com/cogent3/cogent3", True),
+        ("Gallery", "https://cogent3.org/cogent3/draw/index.html", True),
+        ("Projects Using", "projects"),
+        (
+            "Code of Conduct",
+            "https://github.com/cogent3/c3dev/wiki/Code-of-Conduct",
+            True,
+        ),
+    ],
+    "navbar_class": "navbar navbar-inverse",
+    "navbar_fixed_top": "true",
+    "source_link_position": "skipped",
+    "bootswatch_theme": "cerulean",
+    "bootstrap_version": "3",
 }
 
-html_static_path = ["_static"]
+todo_include_todos = True
+todo_emit_warnings = True
 
-htmlhelp_basename = "cogent3doc"
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ["_static"]
 
-# -- Options for Sphinx Gallery
-
-nbsphinx_requirejs_path = "require.js"
-
-
-def plotly_sg_scraper(block, block_vars, gallery_conf, *args, **kwargs):
-    examples_dirs = gallery_conf["examples_dirs"]
-    if isinstance(examples_dirs, (list, tuple)):
-        examples_dirs = examples_dirs[0]
-    pngs = sorted(glob(os.path.join(examples_dirs, "**", "*.png"), recursive=True))
-    htmls = sorted(glob(os.path.join(examples_dirs, "**", "*.html"), recursive=True))
-    image_path_iterator = block_vars["image_path_iterator"]
-    image_names = list()
-    seen = set()
-    for html, png in zip(htmls, pngs):
-        if png not in seen:
-            seen |= {png}
-            this_image_path_png = next(image_path_iterator)
-            this_image_path_html = os.path.splitext(this_image_path_png)[0] + ".html"
-            image_names.append(this_image_path_html)
-            shutil.move(png, this_image_path_png)
-            shutil.move(html, this_image_path_html)
-    # Use the `figure_rst` helper function to generate rST for image files
-    from plotly.io._sg_scraper import figure_rst
-
-    return figure_rst(image_names, gallery_conf["src_dir"])
-
-
-def plotly_sg_scraper_nb(*args, **kwargs):
-    try:
-        result = plotly_sg_scraper(*args, **kwargs)
-    except Exception:
-        result = ""
-    return result
-
-
-image_scrapers = (plotly_sg_scraper_nb,)
-
-example_dirs = ["draw_examples"]
-gallery_dirs = ["draw"]
-
-
-sphinx_gallery_conf = {
-    # "doc_module": ("plotly",),
-    "examples_dirs": example_dirs,
-    "subsection_order": ExplicitOrder(["draw_examples/aln", "draw_examples/tree"]),
-    "abort_on_example_error": True,
-    "within_subsection_order": FileNameSortKey,
-    "gallery_dirs": gallery_dirs,
-    # "reference_url": {"plotly": None,
-    # },
-    "image_scrapers": image_scrapers,
-    "download_all_examples": False,
-}
-
-# -- Options for LaTeX output --------------------------------------------------
-latex_documents = [
-    ("index", "cogent3.tex", "cogent3 Documentation", "cogent3 Team", "manual")
-]
+# sidebar_collapse = False
